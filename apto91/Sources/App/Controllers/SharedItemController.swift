927b1e4adc7e9c1505fcb51d5f449cdb92f6a000
Henrique Alves Batochi <henrique.alvesbatochi@gmail.com>
2024-02-17 22:45:24 -0300

    Added postSharedItem logic

--- apto91/Sources/App/Controllers/SharedItemController.swift
+++ apto91/Sources/App/Controllers/SharedItemController.swift
@@ -21,9 +21,45 @@ struct SharedItemController: RouteCollection {
         try SharedItemRequest.validate(content: req)
         let sharedItem = try req.content.decode(SharedItemRequest.self)
         
-        
-        
-        return PIC001Result(cd_erro: 0, ds_erro: "", nr_item: 0)
+        do {
+            
+            let seqRow = try await DatabaseManager.shared.query(query: """
+                                                                       SELECT * FROM IC.PIC001(\(ProcessInfo.processInfo.environment["ENT_NR_VRS"]!),
+                                                                                               '\("I")',
+                                                                                               \(sharedItem.nrHouse),
+                                                                                               \(sharedItem.nrResident),
+                                                                                               NULL,
+                                                                                               '\(sharedItem.vcItem)')
+                                                                       """)
+            
+            guard let row = seqRow else {
+                throw Abort(.internalServerError)
+            }
+            
+            var response = PIC001Result(cd_erro: 0,
+                                        ds_erro: "",
+                                        nr_item: nil)
+            
+            for try await (cd_erro, ds_erro, nr_item) in row.decode((Decimal, String, Int?).self) {
+                
+                response = PIC001Result(cd_erro: cd_erro,
+                                        ds_erro: ds_erro,
+                                        nr_item: nr_item)
+            }
+            
+            if response.cd_erro == -1 {
+                throw DatabaseManagerError.dbError(response.ds_erro)
+            }
+            
+            return response
+            
+        } catch DatabaseManagerError.dbError(let errorMessage) {
+            throw Abort(.custom(code: 400,
+                                reasonPhrase: errorMessage))
+        } catch {
+            req.logger.error(Logger.Message(stringLiteral: error.localizedDescription))
+            throw error
+        }
     }
     
     func putSharedItem(req: Request) async throws -> PIC001Result {
